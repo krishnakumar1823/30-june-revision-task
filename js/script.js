ebdd039b1644e59af4d9b65544325d088eab576a
krishnakumar1823 <kumarkrishna180320@gmail.com>
2023-06-30 14:08:48 +0530

    add

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// slicing 
+
+// a=prompt("Enter some input to slice");
+// b=a.length;
+// for (i=0;i<b;i++){
+	// document.write(a[i],"<br>");
+// }
+// document.write("length of the given input is =",b);
+
+// square and cube
+
+// a=parseInt(prompt("Enter the number"));
+// if (a){
+	// document.write("Square of the given number = ",a,"  is equal to = ",a*a,"<br>");
+  // document.write("cube of the given number = ",a," is equal to = ",a*a*a);
+// }
+// else{
+	// document.write("Invalid input")
+// }
+
+// swap a number
+// let a=parseInt(prompt("Enter the number A"));
+// let b=parseInt(prompt("Enter the number B"));
+// let c=a;
+// let d=b;
+// if (a && b){
+// document.write("Original input A =",c,", B =",d,"<br>");
+// document.write("After number swapped A = ",(a=d),", B = ",(b=c));
+// }
+// else{
+	// document.write("Invalid Input")
+// }
